b3b3ff2825f93027537e794a5d9bc40514ec48a5
Jeremy Baker <jeremy@retailzipline.com>
2023-01-26 14:23:25 -0600

    Track the activejob queue adapter

--- app/jobs/first_job.rb
+++ app/jobs/first_job.rb
@@ -2,6 +2,6 @@ class FirstJob < ApplicationJob
   def perform
     SecondJob.perform_later
 
-    Rails.logger.warn "FirstJob - Tenant: #{Apartment::Tenant.current}"
+    Rails.logger.warn "FirstJob tenant=#{Apartment::Tenant.current} queue_adapter=#{self.class.queue_adapter}"
   end
 end
--- app/jobs/second_job.rb
+++ app/jobs/second_job.rb
@@ -1,5 +1,5 @@
 class SecondJob < ApplicationJob
   def perform
-    Rails.logger.warn "SecondJob - Tenant: #{Apartment::Tenant.current}"
+    Rails.logger.warn "SecondJob tenant=#{Apartment::Tenant.current} queue_adapter=#{self.class.queue_adapter}"
   end
 end
