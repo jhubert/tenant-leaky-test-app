bf4c35d2af6344d2ee99947a6284de3af7654913
Jeremy Baker <jeremy@retailzipline.com>
2023-01-27 08:54:49 -0800

    Switch to using an sqlite3 database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Apartment::Tenant.create('first') unless Apartment.connection.schema_exists?('first')
+Apartment::Tenant.create('first')
