64cafce6987ccae7c8e8631757fdd6f0e2a4adcb
Jeremy Baker <jeremy@retailzipline.com>
2023-01-27 08:57:16 -0800

    Add the process id to the logger output

--- app/jobs/first_job.rb
+++ app/jobs/first_job.rb
@@ -2,6 +2,6 @@ class FirstJob < ApplicationJob
   def perform
     SecondJob.perform_later
 
-    Rails.logger.warn "FirstJob tenant=#{Apartment::Tenant.current} queue_adapter=#{self.class.queue_adapter}"
+    Rails.logger.warn "FirstJob tenant=#{Apartment::Tenant.current} queue_adapter=#{self.class.queue_adapter_name} pid=#{Process.pid}"
   end
 end
--- app/jobs/second_job.rb
+++ app/jobs/second_job.rb
@@ -1,5 +1,5 @@
 class SecondJob < ApplicationJob
   def perform
-    Rails.logger.warn "SecondJob tenant=#{Apartment::Tenant.current} queue_adapter=#{self.class.queue_adapter}"
+    Rails.logger.warn "SecondJob tenant=#{Apartment::Tenant.current} queue_adapter=#{self.class.queue_adapter_name} pid=#{Process.pid}"
   end
 end
